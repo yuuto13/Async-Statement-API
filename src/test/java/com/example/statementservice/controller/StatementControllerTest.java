7940a210f215028c6f1d24dbf1953fc8caefded4
yuuto <yuutojurgen@gmail.com>
2025-02-11 01:36:23 +0800

    Improve tests

--- src/test/java/com/example/statementservice/controller/StatementControllerTest.java
+++ src/test/java/com/example/statementservice/controller/StatementControllerTest.java
@@ -1,28 +1,73 @@
 package com.example.statementservice.controller;
 
+import com.example.statementservice.cache.TaskCache;
+import com.example.statementservice.model.TaskResult;
+import com.example.statementservice.model.TaskStatus;
+import com.example.statementservice.service.StatementService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = StatementController.class)
+//@AutoConfigureMockMvc(addFilters = false)
+//@ExtendWith(MockitoExtension.class)
 class StatementControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private StatementService statementService;
+
+    @MockitoBean
+    private TaskCache taskCache;
+
     @Test
-    void GenerateStatements_ReturnAllTransactions() throws Exception {
+    void generateStatements_shouldReturnAcceptedWithTaskId() throws Exception {
         mockMvc.perform(get("/v1/statements")
                         .param("fromDate", "2024-01-01")
                         .param("toDate", "2024-06-30")
                         .param("accountNumber", "1000000001"))
                 .andExpect(status().isAccepted())
                 .andExpect(jsonPath("$.taskId").exists());
+    }
+
+    @Test
+    void getStatementFromTask_shouldReturnProcessingWhenStatusProcessing() throws Exception {
+        when(taskCache.get("9d6ca36d-d2a8-4541-a66a-55cf37a102a9")).thenReturn(new TaskResult(TaskStatus.PROCESSING, null));
+        mockMvc.perform(get("/v1/statements/9d6ca36d-d2a8-4541-a66a-55cf37a102a9"))
+                .andExpect(status().isAccepted());
+    }
+
+    @Test
+    void getStatementFromTask_shouldReturnPdfAndClearCacheWhenCompleted() throws Exception {
+        String mockPdf = "JVBERi0xLjUKJYCBgoMKMSAwIG9iago8PC9GaWx0ZXIvRmxhdGVEZWNvZGUvRmlyc3QgMTQxL04gMjAvTGVuZ3==";
+        when(taskCache.get("9d6ca36d-d2a8-4541-a66a-55cf37a102a9")).thenReturn(new TaskResult(TaskStatus.COMPLETED, mockPdf));
+        mockMvc.perform(get("/v1/statements/9d6ca36d-d2a8-4541-a66a-55cf37a102a9"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.pdf").value(mockPdf));
+        verify(taskCache).delete("9d6ca36d-d2a8-4541-a66a-55cf37a102a9");
+    }
+
+    @Test
+    void getStatementFromTask_shouldReturnNotFoundForInvalidTaskId() throws Exception {
+        when(taskCache.get("invalid")).thenReturn(null);
+        mockMvc.perform(get("/v1/statements/invalid"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getStatementFromTask_shouldReturn502WhenTaskFailed() throws Exception {
+        when(taskCache.get("failedTask")).thenReturn(new TaskResult(TaskStatus.FAILED, null));
+        mockMvc.perform(get("/v1/statements/failedTask"))
+                .andExpect(status().isBadGateway());
+        verify(taskCache).delete("failedTask");
     }
 }
--- src/test/java/com/example/statementservice/service/StatementServiceTest.java
+++ src/test/java/com/example/statementservice/service/StatementServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.statementservice.service;
 
 import com.example.statementservice.cache.TaskCache;
-import com.example.statementservice.model.TaskResult;
 import com.example.statementservice.model.TaskStatus;
 import com.example.statementservice.model.Transaction;
 import com.example.statementservice.rest.RestClient;
@@ -12,10 +11,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class StatementServiceTest {
@@ -29,18 +28,27 @@ public class StatementServiceTest {
     private StatementService statementService;
 
     @Test
-    public void GenerateStatements_ReturnVoid() {
-        List<Transaction> transactions = List.of(
+    void generateStatements_shouldStoreCompletedResultWhenSuccessful() {
+        String taskId = UUID.randomUUID().toString();
+        List<Transaction> mockTransactions = List.of(
                 new Transaction("010000032", "2024-11-12T00:00:00",
                         "Fund transfer", "D", 100, "Friends Name", "AED"),
                 new Transaction("010000033", "2024-11-12T00:00:00",
                         "Bill Payment", "D", 500, "Friends Name", "AED")
         );
-        String pdf = "JVBERi0xLjUKJYCBgoMKMSAwIG9iago8PC9GaWx0ZXIvRmxhdGVEZWNvZGUvRmlyc3QgMTQxL04gMjAvTGVuZ3==";
-        String taskId = "9d6ca36d-d2a8-4541-a66a-55cf37a102a9";
-        when(restClient.getAllTransactions("1000000001", "2024-01-01", "2024-06-30")).thenReturn(transactions);
-        when(restClient.generatePdf(transactions)).thenReturn(pdf);
-        doNothing().when(taskCache).put(taskId, new TaskResult(TaskStatus.COMPLETED, pdf));
-        assertAll(() -> statementService.generateStatements(taskId, "2024-01-01", "2024-06-30", "1000000001"));
+        String mockPdf = "JVBERi0xLjUKJYCBgoMKMSAwIG9iago8PC9GaWx0ZXIvRmxhdGVEZWNvZGUvRmlyc3QgMTQxL04gMjAvTGVuZ3==";
+        when(restClient.getAllTransactions(anyString(), anyString(), anyString())).thenReturn(mockTransactions);
+        when(restClient.generatePdf(mockTransactions)).thenReturn(mockPdf);
+        statementService.generateStatements(taskId, "2024-01-01", "2024-06-30", "1000000001");
+        verify(taskCache).put(eq(taskId), argThat(
+                result -> result.status() == TaskStatus.COMPLETED && result.pdfBase64().equals(mockPdf)));
+    }
+
+    @Test
+    void generateStatements_shouldStoreFailedResultOnException() {
+        String taskId = UUID.randomUUID().toString();
+        when(restClient.getAllTransactions(anyString(), anyString(), anyString())).thenThrow(new RuntimeException("Mock error"));
+        statementService.generateStatements(taskId, "2024-01-01", "2024-06-30", "1000000001");
+        verify(taskCache).put(eq(taskId), argThat(result -> result.status() == TaskStatus.FAILED));
     }
 }
